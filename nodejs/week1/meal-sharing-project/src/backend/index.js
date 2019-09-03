cc10c30863090be3bfc4b65ce250c8b1c07cc543
Lilla <korosililla@gmail.com>
2019-09-03 11:27:38 +0200

    everything works

--- nodejs/week1/meal-sharing-project/src/backend/index.js
+++ nodejs/week1/meal-sharing-project/src/backend/index.js
@@ -1,48 +1,23 @@
+// Variables
+const express = require ('express');
+const app = express ();
 
-const express = require('express');
-const app = express();
-const fs = require('fs');
+// Routers
+const mealsRouter = require ('./routes/meals.js');
+const reservationRouter = require ('./routes/reservations.js');
+const cheapMealsRouter = require ('./routes/cheapMeals.js');
+const largeMealsRouter = require ('./routes/largeMeals.js');
+const randomMealRouter = require ('./routes/randomMeal.js');
+const randomReservationRouter = require ('./routes/randomReserv.js');
 
-const mealsRouter = require('./routes/meals.js');
-const reservationRouter = require('./routes/reservations.js');
-const cheapMealsRouter = require('./routes/cheapMeals.js');
-const largeMealsRouter = require('./routes/largeMeals.js');
-const randomMealRouter = require('./routes/randomMeal.js');
+app.use ('/meals', mealsRouter);
+app.use ('/reservations', reservationRouter);
+app.use ('/cheapmeals', cheapMealsRouter);
+app.use ('/largemeals', largeMealsRouter);
+app.use ('/meal', randomMealRouter);
+app.use ('/reservation', randomReservationRouter);
 
-
-app.use('/meals', mealsRouter);
-app.use('/reservations', reservationRouter);
-app.use('/cheapmeals', cheapMealsRouter);
-app.use('/largemeals', largeMealsRouter);
-app.use('/meal', randomMealRouter);
-
-
-
-
-
-
-
-
-
-
-
-// // Respond with the json for a random reservation
-// const randomReservation = reservations[Math.floor(Math.random() * reservations.length)];
-// // console.log(randomReservation);
-
-// app.get('/reservation', (req, res) => {
-//     res.send(randomReservation);
-// })
-
-
-
-
-
-
-
-
-
-
-const server = app.listen(8082, function() {
-  console.log("The app is listening");
-})
+// Server
+const server = app.listen (3000, function () {
+  console.log ('The app is listening at 3000');
+});
