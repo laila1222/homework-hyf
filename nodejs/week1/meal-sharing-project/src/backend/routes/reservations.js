d7f654871dcbe7b3098cf628c21f62628d3de07a
Lilla <korosililla@gmail.com>
2019-09-03 11:18:21 +0200

    update

---
+++ nodejs/week1/meal-sharing-project/src/backend/routes/reservations.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
+
+// //Read and parse reservations.json file
+let rawDataReservations = fs.readFileSync(__dirname + '/../data/reservation.json', 'utf8');
+let reservations = JSON.parse(rawDataReservations);
+// console.log(reservations);
+
+// // Respond with the json for all reservations
+router.get('/', (req, res) => {
+    res.json(reservations);
+})
+
+module.exports = router;
