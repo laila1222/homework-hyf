32333aa9e5dc7d0cdc03a348b875a129c59edae6
Lilla <korosililla@gmail.com>
2019-09-20 12:47:55 +0200

    multiply and divide works

--- nodejs/week3/calculator/index.js
+++ nodejs/week3/calculator/index.js
@@ -13,6 +13,29 @@ app.get('/calculator/multiply', (req, res) => {
     res.send(multiplication.toString());
 })
 
+
+app.get('/calculator', (req, res) => {
+    const key = req.query.key;
+    const numbers = [];
+    let response;
+
+    for (const param in req.query) {
+        const number = Number(req.query[param]);
+        // Push only numbers to the array
+        if (!isNaN(number)) {
+            numbers.push(number);
+        }  
+    }
+
+    if (key === 'multiply') {
+        response = (numbers.reduce((total, amount) => total * amount)).toString();
+    } else if (key === 'division') {
+        response = (numbers.reduce((total, amount) => total / amount)).toString();
+    }
+
+    res.send(response);
+})
+
 app.listen(3002, () => {
     console.log('Server is listening at 3002');
 });
