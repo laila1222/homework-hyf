d26f1e065bdf479a3e8e29beeb2e86959f30d4cf
Lilla <korosililla@gmail.com>
2019-09-20 21:55:08 +0200

    added reservation paths

--- nodejs/week3/meal-sharing/homework/src/backend/index.js
+++ nodejs/week3/meal-sharing/homework/src/backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 
 // Routers
 const mealsRouter = require("./api/meals");
+const reservationsRouter = require('./api/reservations.js');
 const router = express.Router();
 
 const port = process.env.PORT || 5000;
@@ -13,12 +14,13 @@ const buildPath = path.join(__dirname, "../../dist");
 app.use(express.static(buildPath)); */
 
 // Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
 // If path= meals, use mealsRouter
 router.use("/meals", mealsRouter);
+router.use('/reservations', reservationsRouter);
 
 
 
