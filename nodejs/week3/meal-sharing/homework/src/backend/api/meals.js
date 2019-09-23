79125bdde41374b40c0e3511ec3aa3cfd1a097fd
Lilla <korosililla@gmail.com>
2019-09-23 11:17:10 +0200

    queries work seperateli in get /meals

--- nodejs/week3/meal-sharing/homework/src/backend/api/meals.js
+++ nodejs/week3/meal-sharing/homework/src/backend/api/meals.js
@@ -2,59 +2,82 @@ const express = require ('express');
 const router = express.Router ();
 const pool = require ('./../database');
 const bodyParser = require ('body-parser');
+const url = require('url');
 
 router.use (bodyParser.json ());
 
-
 // I am still trying to fix this part, in order to be able to use more queries at the same time
 router.get ('/', (req, res) => {
-  // const availableReservations = req.query.availableReservations;
-  // if (availableReservations) {
-  //   pool.query('select distinct * from meal inner join reservation on meal.id = reservation.meal_id where meal.max_reservations > reservation.number_of_guests;',(err, results, fields) => {
-  //     if (err) {
-  //       console.error(err);
-  //     } else {
-  //       res.send(results);
-  //     }
-  //   })
-  // };
-  // // Sort by maxPrice
-  // const maxPrice = req.query.maxPrice;
-  // pool.query("select * from meal where price <= ? and ;", maxPrice, (error, results, fields) => {
-  // if (error) {
-  //   console.error(error);
-  // } else {
-  //   res.send(results);
-  // }
-  // });
-  // Sort by title
-  // const title = `%${req.query.title}%`;
-  // console.log(title);
-  // pool.query( 'select * from meal where title like ?;', title, (err, results, fields) => {
-  //   if (err) {
-  //     console.error(err);
-  //   } else {
-  //     res.send(results);
-  //   }
-  // });
-  // Meal created after a specific date
-  // const date = req.query.createdAfter;
-  // pool.query('select * from meal where created_date > ?;', date, (err, results, fields) => {
-  //   if (err) {
-  //     console.error(err);
-  //   } else {
-  //     res.send(results);
-  //   }
-  // });
-  // Limit
-  // const limit = Number(req.query.limit);
-  // pool.query('select * from meal limit ?;', limit, (err, results, fields) => {
-  //   if (err) {
-  //     console.error(err);
-  //   } else {
-  //     res.send(results);
-  //   }
-  // });
+  const availableReservations = req.query.availableReservations;
+  const maxPrice = Number(req.query.maxPrice);
+
+  const rawTitle = req.query.title;
+  const title = rawTitle && `%${rawTitle}%`;
+
+  const rawDate = req.query.createdAfter;
+  const date = rawDate && new Date (rawDate + 'Z');
+
+  const limit = Number(req.query.limit);
+
+  // Available reservations
+  if (availableReservations) {
+    pool.query('select distinct * from meal inner join reservation on meal.id = reservation.meal_id where meal.max_reservations > reservation.number_of_guests;',(err, results, fields) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.send(results);
+      }
+    })
+
+  } else if (maxPrice) {
+    // Sort by maxPrice
+    pool.query("select * from meal where price <= ?;", maxPrice, (error, results, fields) => {
+      if (error) {
+        console.error(error);
+      } else {
+        res.send(results);
+      }
+      });
+
+  }  else if (date) {
+    // Meal created after a specific date
+    pool.query('select * from meal where created_date > ?;', date, (err, results, fields) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.send(results);
+      }
+    });
+    
+  } else if (title) {
+    // Sort by title
+    pool.query( 'select * from meal where title like ?;', title, (err, results, fields) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.send(results);
+      }
+    });
+
+  }  else if (limit) {
+    // Limit
+    pool.query('select * from meal limit ?;', limit, (err, results, fields) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.send(results);
+      }
+    });
+
+  } else {
+    pool.query('select * from meal;', (err, results, fields) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.send(results);
+      }
+    });
+  }
 });
 
 // Post new meal
@@ -129,7 +152,6 @@ router.delete ('/:id', (req, res) => {
 // Get meals that has smaller price than maxPrice
 router.get ('/', (req, res) => {
   const maxPrice = req.query.maxPrice;
-  console.log ('hello');
   pool.query (
     'select * from meal where price <= ?',
     maxPrice,
