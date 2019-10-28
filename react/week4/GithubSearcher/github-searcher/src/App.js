9efa16f8b41826ba42e38228f67615836566f45c
Lilla <korosililla@gmail.com>
2019-10-28 10:13:01 +0100

    added Headline

--- react/week4/GithubSearcher/github-searcher/src/App.js
+++ react/week4/GithubSearcher/github-searcher/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Headline from './components/Headline';
 import Users from './components/Users';
 
 function App() {
   return (
     <div>
+      <Headline />
       <Users />
     </div>
   );
---
+++ react/week4/GithubSearcher/github-searcher/src/components/Headline.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Headline () {
+    return ( <h1>Github User Searcher</h1>
+    )
+};
